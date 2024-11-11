6b1c4e08d4c223713c809e5ff5fb2262e22c7573
Bogdan Temnik <bogdantemnikconnect@gmail.com>
2024-11-11 15:38:41 +0100

    change logging password database

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost"; // CHANGE TO YOUR LOCAL
 $username = "root"; // CHANGE TO YOUR LOCAL
-$password = "root"; // CHANGE TO YOUR LOCAL
+$password = ""; // CHANGE TO YOUR LOCAL
 $db = "informaticavosql";
 
 $con = new mysqli($servername, $username, $password, $db);
